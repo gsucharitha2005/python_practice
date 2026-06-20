7254cc720381cb8506c658f855477c10f1a90ba3
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-06-20 22:23:24 +0530

    checking given string is valid or not

---
+++ check_given_string_is_valid.py
@@ -0,0 +1,7 @@
+#writ a program to check given string is valid or not but string character is > 7 
+password=input("enter password:")
+
+if len(password)>7:
+	print("valid password")
+else:
+	print("invalid")
